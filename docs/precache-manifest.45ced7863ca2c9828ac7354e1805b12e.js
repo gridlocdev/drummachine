66a57d9067b8e87e8cf19792f78ec3f4f4fde4c6
gridlocdev <60323737+gridlocdev@users.noreply.github.com>
2021-03-12 17:09:08 -0700

    Added PWA Support

---
+++ docs/precache-manifest.45ced7863ca2c9828ac7354e1805b12e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cc126bb9ae7a6147c9086c2f08230ea",
+    "url": "/drummachine/index.html"
+  },
+  {
+    "revision": "faba4e16d624bd6a0610",
+    "url": "/drummachine/js/app.30fa038f.js"
+  },
+  {
+    "revision": "a603c4122c4aa84956d9",
+    "url": "/drummachine/js/chunk-vendors.f7ee6abc.js"
+  },
+  {
+    "revision": "177bf64566e45256cbba4ec66ed1a351",
+    "url": "/drummachine/manifest.json"
+  },
+  {
+    "revision": "d2e5d4943afa0cc78099e6d2a09b35e4",
+    "url": "/drummachine/media/1 - 808 1.d2e5d494.mp3"
+  },
+  {
+    "revision": "931c953a0036cd5b605e2957e6de79ce",
+    "url": "/drummachine/media/1 - Loop.931c953a.mp3"
+  },
+  {
+    "revision": "021a9a4d908387433210634f01bab70a",
+    "url": "/drummachine/media/2 - 808 2.021a9a4d.mp3"
+  },
+  {
+    "revision": "32207906a2767f1afbceb81f03f8850b",
+    "url": "/drummachine/media/2 - Loop.32207906.mp3"
+  },
+  {
+    "revision": "6425fd8125a4ef76bbeadae0c3a2657e",
+    "url": "/drummachine/media/3 - Loop.6425fd81.mp3"
+  },
+  {
+    "revision": "6681fd8729258d1f9e3b4f8e37135777",
+    "url": "/drummachine/media/3 - Triangle.6681fd87.mp3"
+  },
+  {
+    "revision": "63c4e79ba655bf4b861fa20e31c28c43",
+    "url": "/drummachine/media/4 - Loop.63c4e79b.mp3"
+  },
+  {
+    "revision": "c73025eba9ede4f6252fd4aec508b614",
+    "url": "/drummachine/media/5 - Loop.c73025eb.mp3"
+  },
+  {
+    "revision": "137772d10d1b9e177dc2f51d88ec59b0",
+    "url": "/drummachine/media/6 - Clap.137772d1.mp3"
+  },
+  {
+    "revision": "1e2a2fed80a0dcb92220890cbe9427f5",
+    "url": "/drummachine/media/6 - Loop.1e2a2fed.mp3"
+  },
+  {
+    "revision": "0147ab32532f4d8218f1d54f4d8c929e",
+    "url": "/drummachine/media/7 - Loop.0147ab32.mp3"
+  },
+  {
+    "revision": "a84b1b04a4ae33d88badfe15cb435f84",
+    "url": "/drummachine/media/8 - Loop.a84b1b04.mp3"
+  },
+  {
+    "revision": "f405081f2335fe2a304e7e517a4fdf6d",
+    "url": "/drummachine/media/9 - Loop.f405081f.mp3"
+  },
+  {
+    "revision": "eb770958ed20a1ba030c4b1c0bcddddb",
+    "url": "/drummachine/media/9 - Snare.eb770958.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/drummachine/robots.txt"
+  }
+]);
--- vue.config.js
+++ vue.config.js
@@ -6,5 +6,20 @@ module.exports = {
     ],
     css: {
         extract: false
+    },
+    pwa: {
+        name: "Drum Machine",
+        themeColor: '#000000',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: 'black',
+
+        // // configure the workbox plugin
+        // workboxPluginMode: 'InjectManifest',
+        // workboxOptions: {
+        //     // swSrc is required in InjectManifest mode.
+        //     swSrc: 'dev/sw.js',
+        //     // ...other Workbox options...
+        // }
     }
 }
